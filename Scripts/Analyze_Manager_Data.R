6f228091457dacc593d307a0aafbe2be9773103f
Matt Denny <matthewjdenny@gmail.com>
2015-09-24 15:19:05 -0400

    Added manager networks data to repo

---
+++ Scripts/Analyze_Manager_Data.R
@@ -0,0 +1,14 @@
+# analyze manager-to-manager email networks in 17 county governments
+#works on Matt's Computer
+
+# load data
+rm(list = ls())
+setwd("./Data")
+load("Manager_Email_Networks.Rdata")
+
+#load packages
+library(igraph)
+
+
+
+
