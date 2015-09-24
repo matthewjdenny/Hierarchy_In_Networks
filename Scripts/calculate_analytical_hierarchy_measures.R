350d93874be6bab41764e05ba61e1be98b4b0630
Matt Denny <matthewjdenny@gmail.com>
2015-09-24 15:24:16 -0400

    outline of analytical measure calculation function

---
+++ Scripts/calculate_analytical_hierarchy_measures.R
@@ -0,0 +1,8 @@
+# function to calculate analytical metrics of hierarchy on a sociomatrix
+
+calculate_analytical_hierarhy_measures <- function(sociomatrix){
+
+
+    # return a list object with a $global and $local sublist, each of which contains the output from all of the different measures which are appropriately named. In the $local sublist, we provide the $rank and $score for each node in the network.
+    return(NULL)
+}
