904f227c82d857f2b7fb6effd5417c67a9a4e4d7
Matt Denny <matthewjdenny@gmail.com>
2015-09-24 16:58:03 -0400

    calculate on all networks

--- Scripts/Analyze_Manager_Data.R
+++ Scripts/Analyze_Manager_Data.R
@@ -10,8 +10,18 @@ source("./Scripts/calculate_analytical_hierarchy_measures.R")
 #load data
 load("./Data/Manager_Email_Networks.Rdata")
 
+# calculate measures for all networks
+Measures <- vector(length = length(Manager_Email_Networks), mode = "list")
+for(i in 1:length(Manager_Email_Networks)){
+    print(i)
+    Measures[[i]] <- calculate_analytical_hierarhy_measures(
+        sociomatrix = Manager_Email_Networks[[i]]$sociomatrix,
+        mode = Manager_Email_Networks[[i]]$mode
+                                                            )
+}
 
 
+#now score against
 
 
 
