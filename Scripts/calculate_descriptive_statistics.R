2eec3d90206c73e1b50fc6ffe80fc31f1dc7258b
Matt Denny <matthewjdenny@gmail.com>
2015-10-22 10:18:17 -0400

    calculate descriptive statistics for one network and for each type

---
+++ Scripts/calculate_descriptive_statistics.R
@@ -0,0 +1,31 @@
+calculate_descriptive_statistics <- function(Network_Data){
+
+    local_statistics <- matrix(0,nrow = length(Network_Data), ncol = 4)
+    network_types <- rep("",length(Network_Data))
+    for(i in 1:length(Network_Data)){
+        sociomatrix <- graph.adjacency(Network_Data[[i]]$sociomatrix,
+                                       mode = Network_Data[[i]]$mode)
+        local_statistics[i,1] <- nrow(Network_Data[[i]]$sociomatrix)
+        local_statistics[i,2] <- sum(Network_Data[[i]]$sociomatrix)
+        local_statistics[i,3] <- igraph::graph.density(sociomatrix)
+        local_statistics[i,4] <- igraph::transitivity(sociomatrix, type="global")
+        network_types[i] <- Network_Data[[i]]$type
+    }
+    rownames(local_statistics) <- names(Network_Data)
+    colnames(local_statistics) <- c("nodes","edges","density","clustering_coefficient")
+    local_statistics <- data.frame(local_statistics)
+
+    #calculate type averages
+    unique_types <- unique(network_types)
+    type_statistics <- matrix(0,nrow = length(unique_types), ncol = 4)
+    for(i in 1:length(unique_types)){
+        cur <- local_statistics[which(network_types == unique_types[i]),]
+        type_statistics[i,] <- colMeans(cur)
+    }
+    rownames(type_statistics) <- unique_types
+    colnames(type_statistics) <- c("nodes","edges","density","clustering_coefficient")
+    type_statistics <- data.frame(type_statistics)
+
+    return(list(network_statistics = local_statistics,
+                type_statistics = type_statistics))
+}
