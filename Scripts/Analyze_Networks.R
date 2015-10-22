54a48ee586cc53ec30d59b9b433ad2ea1cac56ee
Matt Denny <matthewjdenny@gmail.com>
2015-10-22 09:27:58 -0400

    condense script to run with new top level function

--- Scripts/Analyze_Networks.R
+++ Scripts/Analyze_Networks.R
@@ -8,44 +8,24 @@ rm(list = ls())
 source("./Scripts/calculate_analytical_hierarchy_measures.R")
 source("./Scripts/score_leadership_rank.R")
 source("./Scripts/multi_plot.R")
+source('./Scripts/generate_hierarchy_dataset.R')
+
+#for better plotting
+#devtools::install_github("matthewjdenny/SpeedReader")
+library(SpeedReader)
+
 
 #load data
 load("./Data/Network_Data.Rdata")
 
-# calculate measures for all networks
-Measures <- vector(length = length(Network_Data), mode = "list")
-for(i in 1:length(Network_Data)){
-    print(i)
-    Measures[[i]] <- calculate_analytical_hierarhy_measures(
-        sociomatrix = Network_Data[[i]]$sociomatrix,
-        mode = Network_Data[[i]]$mode
-    )
-}
+data_list <- generate_hierarchy_dataset(Network_Data)
 
-#populate dataframe with global measures
-global_measures <- data.frame(matrix(0,
-                                     nrow = length(Measures),
-                                     ncol = length(Measures[[1]]$global)))
-for(i in 1:length(Measures)){
-    global_measures[i,] <- unlist(Measures[[i]]$global)
-}
-colnames(global_measures) <- names((Measures[[1]]$global))
-rownames(global_measures) <- names(Network_Data)
-
-save(global_measures, file = "./Data/global_hierarchy_measures.Rdata")
-
-multi_plot(data = global_measures,
+multi_plot(data = data_list$global_measure_dataframe,
            pdf_name = "Global_Measures",
            output_pdf = F,
            c(2:5,7))
 
-
-#now score against their leadership rank
-measure_scores <- score_leadership_rank(
-    Network_Data = Network_Data,
-    Measures = Measures)
-
-multi_plot(data = measure_scores,
+multi_plot(data = data_list$leadership_ranking_scores,
            pdf_name = "Measure_Scores",
            output_pdf = F)
 
