0561250aa1a87a7e383c7c0e7746998ef007ec91
cmcook22 <cmcook22@gmail.com>
2015-10-28 10:19:06 -0400

    Updating Presentation
    added new descriptive stats for networks, new multiplots for ranking,
    and new pca plots

--- Scripts/Slides_Oct21.R
+++ Scripts/Slides_Oct21.R
@@ -109,3 +109,37 @@ pairs(global_measures[1:7], main = "Global Hierarchy Measures",
       "kendall","m_degree","m_close","GRC","D_root","degree","closeness","betweenness",
           "eigenvector"))
 
+
+Network_Data[-c(100,52:54,128,78)]
+
+
+
+
+data_list <- generate_hierarchy_dataset(Network_Data[-c(100,52:54,128,78)])
+
+global_measures <- data_list$global_measure_dataframe
+#deal with weird networks
+global_measures <- global_measures[-100,]
+global_measures <- global_measures[-which(row.names(global_measures) == "drugnet"),]
+global_measures <- global_measures[-which(row.names(global_measures) == "Terro_4275"),]
+global_measures <- global_measures[-which(row.names(global_measures) == "drug_net_61"),]
+global_measures <- global_measures[-which(row.names(global_measures) == "Koster_data8"),]
+global_measures <- global_measures[-which(row.names(global_measures) == "CITIES"),]
+
+save(global_measures, file = "./Data/global_hierarchy_measures.Rdata")
+
+multi_plot(data = global_measures,
+           pdf_name = "Global_Measures",
+           output_pdf = F,
+           c(2:5,7))
+
+multi_plot(data = data_list$leadership_ranking_scores,
+           pdf_name = "Measure_Scores",
+           output_pdf = F)
+
+# calculate some descriptive statistics
+descriptive_stats <- calculate_descriptive_statistics(Network_Data[-c(100,52:54,128,78)])
+network_descriptive_statistics <- descriptive_stats[[1]]
+type_descriptive_statistics <- descriptive_stats[[2]]
+
+
