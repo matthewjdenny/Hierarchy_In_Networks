375cb1af4f665dddb9321a122007e269f7d0926e
Matt Denny <matthewjdenny@gmail.com>
2015-11-07 18:27:07 -0500

    reorganize main to make replication easier

--- Scripts/Analyze_Networks.R
+++ Scripts/Analyze_Networks.R
@@ -1,5 +1,4 @@
-#calculate measures for all networks
-#works on Matt's Computer
+# Replication script
 
 #preliminaries
 rm(list = ls())
@@ -8,16 +7,23 @@ rm(list = ls())
 # for me, this is:
 setwd("~/Dropbox/SoDA_502/Hierarchy_In_Networks")
 
+# install and load functions
+# devtools::install_github("matthewjdenny/SpeedReader")
+# install_github("vqv/ggbiplot")
+require(SpeedReader)
+require(ggbiplot)
+require(igraph)
+
 #load in functions
 source("./Scripts/calculate_analytical_hierarchy_measures.R")
 source("./Scripts/score_leadership_rank.R")
 source("./Scripts/multi_plot.R")
 source('./Scripts/generate_hierarchy_dataset.R')
 source('./Scripts/calculate_descriptive_statistics.R')
+source('./Scripts/generate_pca_plots.R')
 
-# for better plotting you can try this
-# devtools::install_github("matthewjdenny/SpeedReader")
-# library(SpeedReader)
+
+
 
 #load data
 load("./Data/Network_Data.Rdata")
@@ -25,13 +31,18 @@ load("./Data/Network_Data.Rdata")
 data_list <- generate_hierarchy_dataset(Network_Data)
 
 global_measures <- data_list$global_measure_dataframe
+
+
 #deal with weird networks
-global_measures <- global_measures[-100,]
+global_measures <- global_measures[-which(row.names(global_measures) == "mb031s01nets2"),]
 global_measures <- global_measures[-which(row.names(global_measures) == "drugnet"),]
 global_measures <- global_measures[-which(row.names(global_measures) == "Terro_4275"),]
 global_measures <- global_measures[-which(row.names(global_measures) == "drug_net_61"),]
 global_measures <- global_measures[-which(row.names(global_measures) == "Koster_data8"),]
 global_measures <- global_measures[-which(row.names(global_measures) == "CITIES"),]
+global_measures <- global_measures[-which(row.names(global_measures) == "AOM_division_comembership"),]
+global_measures <- global_measures[-which(row.names(global_measures) == "Freeman's_EIES3"),]
+
 
 save(global_measures, file = "./Data/global_hierarchy_measures.Rdata")
 
@@ -49,5 +60,6 @@ descriptive_stats <- calculate_descriptive_statistics(Network_Data)
 network_descriptive_statistics <- descriptive_stats[[1]]
 type_descriptive_statistics <- descriptive_stats[[2]]
 
-
+# run pca analysis
+generate_pca_plots(global_measures)
 
